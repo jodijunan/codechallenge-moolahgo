44da35f695b607ce1c255e6dd5ecec4da1ebda1a
dondasasmita@gmail.com <dondasasmita@gmail.com>
2019-04-27 00:38:33 +0800

    calculation controller

---
+++ calculation.php
@@ -0,0 +1,28 @@
+<?php
+require "Fee.php";
+
+function calculate($date, $amount, $percentage)
+{
+    $addition = $amount * $percentage;
+    $result = ["date" => $date, "fee" => $amount + $addition];
+    return json_encode($result);
+}
+
+if (isset($_POST["amount"])) {
+    $amount = (int)$_POST["amount"];
+    $percentage = (float)$_POST["percentage"] / 100;
+    $result = calculate($_POST["date"], $amount, $percentage);
+
+    $fee = new Fee();
+
+    $fee->setDate($_POST["date"]);
+    $fee->setArbitraryAmount($_POST["amount"]);
+    $fee->setPercentage($_POST["percentage"]);
+    $fee->setFeeAmount(json_decode($result)->fee);
+  
+    $fee->insert();
+
+    // Get the result fee from database and display
+
+    echo $result;
+}
