a0b405009262a2c3074dec5ef7269c19ad60e7cd
jcfdl <jc.deleon027@gmail.com>
2020-02-17 01:29:37 +0800

    minor security change and validation omitted on my last commit will redo a pull request again

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -20,16 +20,18 @@ class Home extends CI_Controller {
 			'success' => false,
 			'message' => 'No referral person found!'
 		);
-		$user = $this->user->getUser($_GET['referralcode']);
-		if($user) {
-			$data['success'] = true;
-			$data['message'] = 'Referral person found!';
-			// IF I WILL USE JAVASCRIPT TO SHOW CONTENT
-			$data['data'] = $user;
-			// IF I WILL FORMAT HERE FOR EASIER DISPLAYING
-			// $data['data'] = "<p>{$data['message']}</p>";
-			// $data['data'] .= "<p><strong>Referral Person:</strong> {$user['first_name']} {$user['last_name']}</p>";
-			// $data['data'] .= "<p><strong>Address:</strong> {$user['address']}</p>"; 
+		if(isset($_GET['referralcode'])) {
+			$user = $this->user->getUser($_GET['referralcode']);
+			if($user) {
+				$data['success'] = true;
+				$data['message'] = 'Referral person found!';
+				// IF I WILL USE JAVASCRIPT TO SHOW CONTENT
+				$data['data'] = $user;
+				// IF I WILL FORMAT HERE FOR EASIER DISPLAYING
+				// $data['data'] = "<p>{$data['message']}</p>";
+				// $data['data'] .= "<p><strong>Referral Person:</strong> {$user['first_name']} {$user['last_name']}</p>";
+				// $data['data'] .= "<p><strong>Address:</strong> {$user['address']}</p>"; 
+			}
 		}
 		echo json_encode($data);
 	}
