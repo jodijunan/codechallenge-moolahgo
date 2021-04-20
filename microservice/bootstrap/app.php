f0e0c2cc5b5484378e4f3cf43fa24d9b2e09082b
Ahadian Akbar <rabkawork@gmail.com>
2021-04-20 15:34:04 +0700

    add cors middleware

--- microservice/bootstrap/app.php
+++ microservice/bootstrap/app.php
@@ -72,9 +72,10 @@ $app->configure('app');
 |
 */
 
-// $app->middleware([
+ $app->middleware([
 //     App\Http\Middleware\ExampleMiddleware::class
-// ]);
+       App\Http\Middleware\CorsMiddleware::class
+ ]);
 
 // $app->routeMiddleware([
 //     'auth' => App\Http\Middleware\Authenticate::class,
