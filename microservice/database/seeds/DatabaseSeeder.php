a4a841f9b80f4ea53b795dbb33bcff4c8bbce331
Ahadian Akbar <rabkawork@gmail.com>
2021-04-21 10:56:53 +0700

    add db seeder to fill owner and referral code on database

--- microservice/database/seeds/DatabaseSeeder.php
+++ microservice/database/seeds/DatabaseSeeder.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use App\Libraries\Referralcode;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +15,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call('UsersTableSeeder');
+	$startDate = time();
+
+	$randomName = array("Ahadian Akbar","Safrizal","Chalimatus","Diyah","Rizal");
+
+	for($i = 0; $i < 5; $i++){
+		$ownerId = DB::table('owner')->insertGetId([
+	            'name' => $randomName[rand(0, count($randomName)-1)],
+	            'email' => Str::random(10).'@gmail.com',
+	        ]);
+
+		DB::table('referral_code')->insertGetId([
+                    'owner_id' => $ownerId,
+                    'code' => Referralcode::generatecode(6),
+                    'status' => 0, // 1 => active, 0 => inactive
+                    'used' => 0, // start from 0
+		    'expired_date' => date('Y-m-d H:i:s', strtotime('+3 day', $startDate))
+                ]);
+	}
     }
 }
