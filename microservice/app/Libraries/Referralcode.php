6fbc5acb214b1767fd5fd54b511886b959254e7e
Ahadian Akbar <rabkawork@gmail.com>
2021-04-21 10:56:18 +0700

    add library -> for generate referral code

--- microservice/app/Libraries/Referralcode.php
+++ microservice/app/Libraries/Referralcode.php
@@ -6,8 +6,8 @@ namespace App\Libraries;
 
 class Referralcode {
 
-  public function generatecode($len) {
-     $str_result = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890abcdefghijklmnopqrstuvwxyz'; 
+  public static function generatecode($len) {
+     $str_result = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'; 
     return substr(str_shuffle($str_result), 0, $len);
   }
 
