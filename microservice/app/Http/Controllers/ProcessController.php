3f9794bae80e99ec3cdcb2e54fa085a55fad6257
Ahadian Akbar <rabkawork@gmail.com>
2021-04-21 22:58:59 +0700

    add condition -> to check expired date {referral code}

--- microservice/app/Http/Controllers/ProcessController.php
+++ microservice/app/Http/Controllers/ProcessController.php
@@ -1,12 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
-
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Validator;
-use Illuminate\Support\Facades\Auth;
 use App\ReferralCode;
 
 class ProcessController extends Controller
@@ -23,24 +18,44 @@ class ProcessController extends Controller
 
             if ($validator->fails()) {
                 return response()->json([
-                        'msg'      => $validator->errors(),
-                        'status'   => 404
+                        'msg'    => 'Input invalid!',
+                        'data'   => $validator->errors(),
+                        'status' => 404
                         ],
                        404);
             } else {
                 $refCode = new ReferralCode();
-                $find = $refCode->with('owner')->where('code',$input)->first();
+                $result  = array();
 
+                if(!empty($input['showall']) && isset($input['showall'])) {
+                    $result = $refCode->with('owner')->all();
+                } else {
+                    $result = $refCode->with('owner')->where('code',$input['code'])->first();
+                    
+                    $today       = date("Y-m-d H:i:s");  
+                    $today_date  = new \DateTime($today);
+                    $expiredDate = new \DateTime($result->expired_date);
+
+                    if($today_date < $expiredDate){
+                        return response()->json([
+                            'msg'    => 'Expired referral code!',
+                            'data'   => $result,
+                            'status' => 401
+                            ],200);
+                    }
+                }
+                
                 return response()->json([
-                    'msg'      => $find,
-                    'status'   => 200
-                    ],
-                   200);
+                    'msg'    => 'Data Found!',
+                    'data'   => $result,
+                    'status' => 200
+                    ],200);
             }
         } catch(Exception $e) {
             return response()->json([
-                'msg'      => 'Internal Server Error',
-                'status'   => 500,
+                'msg'    => 'Input invalid!',
+                'data'   => NULL,
+                'status' => 500,
             ], 200);
         }
     }
